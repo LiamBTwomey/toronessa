8d55eada9aee373502648899a528ae62b49371eb
LiamBTwomey <ltwomey2@gmail.com>
2023-07-23 22:20:55 -0400

    begin count function

--- src/components/body/Test.jsx
+++ src/components/body/Test.jsx
@@ -1,15 +1,21 @@
 import Card from 'react-bootstrap/Card';
 import styles from './About.css';
 import Button from 'react-bootstrap/Button';
-
+import dancingone from './dancingone.jpg'
+import dancingtwo from './dancingtwo.jpg'
 
 function BodyOnlyExample() {
   return (
 <div>
-    <Card><span><br></br>Please click/tap the arrow 16 times.
-<br></br><span>Please turn your sound on</span><br></br><br></br>
-<span>Click/tap the arrow</span><br></br><br></br>
-<Button href="1" variant="dark" size="lg" block>
+    <Card><span><br></br>Please click/tap the @ 16 times.
+<br></br><br></br><img src={dancingone} size="50px 50px" />
+<br></br>
+<Button href="3" variant="dark" size="sm" block>
+        @
+      </Button>
+<br></br>
+<br></br>
+<Button href="1" variant="dark" size="sm" block>
         →
       </Button></span>
 </Card>
@@ -20,4 +26,3 @@ function BodyOnlyExample() {
 }
 
 export default BodyOnlyExample;
-
