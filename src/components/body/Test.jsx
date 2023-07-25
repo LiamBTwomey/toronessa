4f7a8426be70fb37288924e39e636ac91122f2a7
LiamBTwomey <ltwomey2@gmail.com>
2023-07-25 00:54:39 -0400

    counter added

--- src/components/body/Test.jsx
+++ src/components/body/Test.jsx
@@ -3,18 +3,17 @@ import styles from './About.css';
 import Button from 'react-bootstrap/Button';
 import dancingone from './dancingone.jpg'
 import dancingtwo from './dancingtwo.jpg'
+import { useState } from 'react'
 
 function BodyOnlyExample() {
+  const [count, setCount] = useState(0)
   return (
 <div>
     <Card><span><br></br>Please click/tap the @ 16 times.
 <br></br><br></br><img src={dancingone} size="50px 50px" />
 <br></br>
-<Button href="3" variant="dark" size="sm" block>
-        @
-      </Button>
-<br></br>
-<br></br>
+ <Button variant="dark" size="sm" onClick={() => setCount((count) => count + 1)}>
+          @ {count}</Button>
 <Button href="1" variant="dark" size="sm" block>
         →
       </Button></span>
