59103c63c9e97b6a53d413296b0a4686f3f3a45c
LiamBTwomey <ltwomey2@gmail.com>
2023-09-24 02:05:54 -0400

    With GA4

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-66W15M5JSW");
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
